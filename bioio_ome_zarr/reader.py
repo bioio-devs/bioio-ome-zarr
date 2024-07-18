fb3e57531c7bcca8671f4852e4a21d561f6ffd56
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2024-07-18 10:57:09 -0700

    remove unnecessary typing

--- bioio_ome_zarr/reader.py
+++ bioio_ome_zarr/reader.py
@@ -36,7 +36,7 @@ class Reader(reader.Reader):
     _metadata: Optional[Any] = None
     _scenes: Optional[Tuple[str, ...]] = None
     _current_scene_index: int = 0
-    _resolution_level_dict: Optional[Dict[int, Tuple[int, ...]]] = None
+
     # Do not provide default value because
     # they may not need to be used by your reader (i.e. input param is an array)
     _fs: "AbstractFileSystem"
