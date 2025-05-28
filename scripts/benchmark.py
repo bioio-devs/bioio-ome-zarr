4a55620503f5642281bd436f80d91450b1e3550c
SeanLeRoy <seanme97@gmail.com>
2025-05-28 14:52:18 -0700

    Add new function to benchmark test

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -8,5 +8,14 @@ import bioio_base.benchmark
 import bioio_ome_zarr
 
 
+benchmark_functions: bioio_base.benchmark.BenchmarkDefinition = [
+    {
+        "prefix": "Get resolution levels",
+        "test": lambda test_file, Reader: Reader(test_file).resolution_levels,
+    },
+]
+
+
+# This file is under /scripts while the test resourcess are under /bioio_ome_zarr/tests/resources
 test_resources_dir = pathlib.Path(__file__).parent.parent / "bioio_ome_zarr" / "tests" / "resources"
-bioio_base.benchmark.benchmark(bioio_ome_zarr.reader.Reader, test_resources_dir)
+bioio_base.benchmark.benchmark(bioio_ome_zarr.reader.Reader, test_resources_dir, benchmark_functions)
