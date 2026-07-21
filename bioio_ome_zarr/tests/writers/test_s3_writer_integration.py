ce6a7e94e559be80d28c476cb11bca77f4b76cad
Daniel Saelid <daniel.saelid@alleninstitute.org>
2026-07-21 11:49:08 -0500

    lint

--- bioio_ome_zarr/tests/writers/test_s3_writer_integration.py
+++ bioio_ome_zarr/tests/writers/test_s3_writer_integration.py
@@ -3,7 +3,11 @@ from typing import Generator
 from uuid import uuid4
 
 import fsspec
+import numpy as np
 import pytest
+
+from bioio_ome_zarr.tests.writers.test_ome_zarr_writer import assert_valid_ome_zarr
+from bioio_ome_zarr.writers import Channel, OMEZarrWriter
 
 # located in the aics-dev account (771753870375)
 S3_TEST_BUCKET_NAME = "aics-pipeline-output"
@@ -22,7 +26,7 @@ class TestS3WriterIntegration:
 
     @pytest.fixture(scope="class")
     @classmethod
-    def s3_prefix(cls) -> Generator[str]:
+    def s3_prefix(cls) -> Generator[str, None, None]:
         # Setup: Runs once before any tests in the class start
         s3_prefix = f"s3://{S3_TEST_BUCKET_NAME}/{uuid4()}"
 
@@ -50,3 +54,48 @@ class TestS3WriterIntegration:
 
         # Assert
         assert read_data == data_to_write
+
+    @pytest.mark.parametrize("zarr_format", [2, 3])
+    def test_ome_zarr_writer_writes_to_s3(
+        self, s3_prefix: str, zarr_format: int
+    ) -> None:
+        """
+        Test that the OME-Zarr writer can write to S3. Simple example data taken
+        from the README.
+        """
+        # Arrange
+        level_shapes = [
+            (2, 3, 4, 256, 256),  # L0 full res
+            (2, 3, 4, 128, 128),  # L1 downsampled Y/X by 2
+        ]
+
+        data = np.random.randint(0, 255, size=level_shapes[0], dtype=np.uint8)
+        channels = [
+            Channel(label=f"c{i}", color="FF0000") for i in range(data.shape[1])
+        ]
+
+        output_store = f"{s3_prefix}/output_{zarr_format}.zarr"
+
+        kwargs = dict(
+            store=output_store,
+            level_shapes=level_shapes,
+            dtype=data.dtype,
+            zarr_format=zarr_format,  # 2 for Zarr v2
+            channels=channels,
+            axes_names=["t", "c", "z", "y", "x"],
+            axes_types=["time", "channel", "space", "space", "space"],
+            axes_units=[None, None, "micrometer", "micrometer", "micrometer"],
+        )
+        writer = OMEZarrWriter(**kwargs)
+
+        # Act
+        writer.write_full_volume(data)
+
+        # Assert
+        # Check that the output store exists and is a directory
+        fs = fsspec.filesystem("s3")
+        assert fs.exists(output_store)
+        assert fs.isdir(output_store)
+
+        # Validate the OME-Zarr
+        assert_valid_ome_zarr(output_store, zarr_format=zarr_format)
